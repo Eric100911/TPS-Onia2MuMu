b50bfee188fc8ccfeb89e84d008a7ad71803020b
Chi Wang <chiw@lxplus930.cern.ch>
2025-02-05 13:24:05 +0100

    :sparkles: Allow resubmit with multiple error code scenarios.
    :wrench: Give executable mode to some shell scripts.

--- test/crabData/resubmit.sh
+++ test/crabData/resubmit.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-files='crab_crab3_6_*'
+files='crab_crab3_*_*_MINIAOD'
 #for i in $files
 #do
 	#echo -e "\033[32m $i \033[0m"
@@ -8,12 +8,25 @@ files='crab_crab3_6_*'
 	cat tmp_report.out | while read rows
 	do	
 		result=$(echo $rows | grep 'MINIAOD')
+		to_resubmit=0
 		if [[ $result != '' ]]
 		then
 			i=$rows
 			#echo -e "\033[32m $i \033[0m"
 		fi
-		result1=$(echo $rows | grep '50664')
+		result1=$(echo $rows | grep '8001')
+		if [[ $result1 != '' ]]
+		then
+			num=$(echo $result1 | awk '{print $1}')
+			num=$(expr $num)
+			if [[ $num -lt 100 ]]
+			then
+				echo -e "\033[32m $i \033[0m resubmit because $num 8001 failure"
+		#		crab --quiet resubmit $i
+				to_resubmit=1
+			fi
+		fi
+		result2=$(echo $rows | grep '50664')
 		if [[ $result1 != '' ]]
 		then
 			num=$(echo $result1 | awk '{print $1}')
@@ -21,8 +34,25 @@ files='crab_crab3_6_*'
 			if [[ $num -lt 100 ]]
 			then
 				echo -e "\033[32m $i \033[0m resubmit because $num 50664 failure"
-				crab --quiet resubmit $i
+		#		crab --quiet resubmit $i
+				to_resubmit=1
 			fi
+		fi
+		result3=$(echo $rows | grep '8002')
+		if [[ $result1 != '' ]]
+		then
+			num=$(echo $result1 | awk '{print $1}')
+			num=$(expr $num)
+			if [[ $num -lt 100 ]]
+			then
+				echo -e "\033[32m $i \033[0m resubmit because $num 8002 failure"
+		#		crab --quiet resubmit $i
+				to_resubmit=1
+			fi
+		fi
+		if [[ $to_resubmit -eq 1 ]]
+		then
+			crab resubmit $i
 		fi
 	done 
 	rm -f tmp_report.out
