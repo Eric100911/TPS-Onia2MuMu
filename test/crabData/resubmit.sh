6545b2389f915df2caaf9adbecbc321ecc7ce605
Chi Wang <chiw@lxplus915.cern.ch>
2025-02-07 16:48:55 +0100

    :sparkles: Improve resubmit.sh to handle postprocessing issue.

--- test/crabData/resubmit.sh
+++ test/crabData/resubmit.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
-files='crab_crab3_6_*'
-for i in $files
-do
+files='crab_crab3_*_*'
+#for i in $files
+#do
 	#echo -e "\033[32m $i \033[0m"
 	#crab status $i | sed -n -e '/jobs failed/p' > tmp_report.out
 	cat report.out > tmp_report.out
 	cat tmp_report.out | while read rows
 	do	
+		toresubmit=0
 		result=$(echo $rows | grep 'MINIAOD')
 		to_resubmit=0
 		if [[ $result != '' ]]
@@ -14,46 +15,46 @@ do
 			i=$rows
 			echo -e "Checking \033[32m $i \033[0m"
 		fi
-		result1=$(echo $rows | grep '8001')
+		result1=$(echo $rows | egrep '80[0-9]{2,2}')
 		if [[ $result1 != '' ]]
 		then
 			num=$(echo $result1 | awk '{print $1}')
 			num=$(expr $num)
 			if [[ $num -lt 100 ]]
 			then
-				echo -e "\033[32m $i \033[0m resubmit because $num 8001 failure"
-		#		crab --quiet resubmit $i
-				to_resubmit=1
+				echo -e "\033[32m $i \033[0m resubmit because $num 80xx failure"
+				# crab --quiet --siteblacklist=T2_FR_GRIF resubmit $i
+				toresubmit=1
 			fi
 		fi
 		result2=$(echo $rows | grep '50664')
-		if [[ $result1 != '' ]]
+		if [[ $result2 != '' ]]
 		then
-			num=$(echo $result1 | awk '{print $1}')
+			num=$(echo $result2 | awk '{print $1}')
 			num=$(expr $num)
 			if [[ $num -lt 100 ]]
 			then
 				echo -e "\033[32m $i \033[0m resubmit because $num 50664 failure"
-		#		crab --quiet resubmit $i
-				to_resubmit=1
+				# crab --quiet --siteblacklist=T2_FR_GRIF resubmit $i
+				toresubmit=1
 			fi
 		fi
-		result3=$(echo $rows | grep '8002')
-		if [[ $result1 != '' ]]
+		result2=$(echo $rows | grep 'postprocessing')
+		if [[ $result2 != '' ]]
 		then
-			num=$(echo $result1 | awk '{print $1}')
+			num=$(echo $result2 | awk '{print $1}')
 			num=$(expr $num)
 			if [[ $num -lt 100 ]]
 			then
-				echo -e "\033[32m $i \033[0m resubmit because $num 8002 failure"
-		#		crab --quiet resubmit $i
-				to_resubmit=1
+				echo -e "\033[32m $i \033[0m resubmit because $num postprocessing failure"
+				# crab --quiet --siteblacklist=T2_FR_GRIF resubmit $i
+				toresubmit=1
 			fi
 		fi
-		if [[ $to_resubmit -eq 1 ]]
+		if [[ $toresubmit -eq 1 ]]
 		then
-			crab resubmit $i
+			crab --quiet resubmit --siteblacklist=T2_FR_GRIF,T2_US_Wisconsin $i
 		fi
 	done 
 	rm -f tmp_report.out
-done
+#done
