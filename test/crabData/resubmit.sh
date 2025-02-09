31f1b9c04cea64918498d3d4e7d6e8709eebdad2
Chi Wang <chiw@lxplus900.cern.ch>
2025-02-09 09:19:15 +0100

    :sparkles: Added some error code rules to resubmit.sh
    :bug: Resolved grammar issue.

--- test/crabData/resubmit.sh
+++ test/crabData/resubmit.sh
@@ -27,7 +27,7 @@ files='crab_crab3_*_*'
 				toresubmit=1
 			fi
 		fi
-		result2=$(echo $rows | grep '50664')
+		result2=$(echo $rows | egrep '50[0-9]{3,3}')
 		if [[ $result2 != '' ]]
 		then
 			num=$(echo $result2 | awk '{print $1}')
@@ -35,6 +35,18 @@ files='crab_crab3_*_*'
 			if [[ $num -lt 100 ]]
 			then
 				echo -e "\033[32m $i \033[0m resubmit because $num 50664 failure"
+				# crab --quiet --siteblacklist=T2_FR_GRIF resubmit $i
+				toresubmit=1
+			fi
+		fi
+		result2=$(echo $rows | egrep 'code 5$')
+		if [[ $result2 != '' ]]
+		then
+			num=$(echo $result2 | awk '{print $1}')
+			num=$(expr $num)
+			if [[ $num -lt 100 ]]
+			then
+				echo -e "\033[32m $i \033[0m resubmit because $num 5 failure. [Do pay attention!]"
 				# crab --quiet --siteblacklist=T2_FR_GRIF resubmit $i
 				toresubmit=1
 			fi
@@ -54,6 +66,7 @@ files='crab_crab3_*_*'
 		if [[ $toresubmit -eq 1 ]]
 		then
 			crab --quiet resubmit --siteblacklist=T2_FR_GRIF,T2_US_Wisconsin $i
+		fi
 	done 
 	rm -f tmp_report.out
 #done
